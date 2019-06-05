1dc5d311adbd41f9bdd6e7f9c52a70aa725d08bb
Colin Clement <colin.clement@gmail.com>
2019-06-05 16:35:37 -0400

    improved plot

--- tests/annular_currents.py
+++ tests/annular_currents.py
@@ -13,7 +13,7 @@ import numpy as np
 from scipy.special import expit
 import matplotlib.pyplot as plt
 
-from pysquid.util.linear_operators import makeD
+from pysquid.util.linear_operators import curl
 
 def uniform_profile(x, y, ir, w):
     r = np.hypot(x, y)
@@ -24,7 +24,7 @@ def parabolic_profile(x, y, ir, w):
     mask = uniform_profile(x, y, ir, w)
     mask = (mask - (ir + w)/2)**2 - (ir + w) ** 2 / 4
     jx = mask * y / r
-    return - np.cumsum(jx, 0)
+    return np.cumsum(jx, 0)
 
 profiles = {'uniform': uniform_profile, 'parabolic': parabolic_profile}
 
@@ -38,3 +38,23 @@ def annular_gfield(L, inner_rad, width, profile='uniform'):
     xx -= xx.mean()
     yy -= yy.mean()
     return profiles[profile](xx, yy, inner_rad, width)
+
+def simplify(axes):
+    for ax in axes:
+        ax.axis('off')
+
+def plotcurrents(gfield, cross=True):
+    jx, jy = curl(gfield)
+    fig, axes = plt.subplots(1, 4 if cross else 3, figsize=(8.9, 3))
+    kw = dict(vmin=min(jx.min(), jy.min()), vmax=max(jx.max(), jy.max()))
+    axes[0].matshow(gfield, cmap='copper')
+    axes[0].set_title(r'$g$-field')
+    axes[1].matshow(jx, **kw)
+    axes[1].set_title(r'$j_x$')
+    axes[2].matshow(jy, **kw)
+    axes[2].set_title(r'$j_y$')
+    if cross:
+        axes[3].plot(jy[jy.shape[0]//2])
+        axes[3].set_title(r'$j_y$ cross-section')
+    simplify(axes)
+    return fig, axes
