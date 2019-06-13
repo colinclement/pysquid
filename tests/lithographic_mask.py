51e250120f5ecdafde3dcc73d4bf27789ed4a6f8
Colin Clement <colin.clement@gmail.com>
2019-06-13 16:25:19 -0400

    print messages about status

--- tests/lithographic_mask.py
+++ tests/lithographic_mask.py
@@ -21,12 +21,14 @@ mask = 1 * (plt.imread(MASK_FILE)[:,:,3] > 0)[:1000]
 if __name__ == '__main__':
     # slice up the mask so it doesn't take forever!
     model_slice = np.s_[400:, 151:1500]
+    print('Solving currents for mask model')
     netmodel = ResistorNetworkModel(mask[model_slice])
     
     # select field of view
     window = np.s_[430:520, 610:740]
     g = netmodel.gfield[window]
 
+    print('Saving currents')
     np.savez(os.path.join(DIR, 'data', 'g-field-litho.npz'), 
              g=g, model_slice=model_slice, window=window)
 
