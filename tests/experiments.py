189ca57deebebab79d5d691cc0518f9917fb8455
Colin Clement <colin.clement@gmail.com>
2019-07-07 16:37:54 -0400

    improved regularization plot

--- tests/experiments.py
+++ tests/experiments.py
@@ -1,7 +1,9 @@
+import string
 import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import matplotlib.lines as lines
+import matplotlib.gridspec as gridspec
 from scipy.ndimage import binary_erosion
 from mpl_toolkits.axes_grid1 import ImageGrid
 from matplotlib.offsetbox import AnnotationBbox, OffsetImage
@@ -45,86 +47,81 @@ def addline(axe, xy, xytext):
                  textcoords='figure fraction',
                  arrowprops=dict(arrowstyle='-'))
 
-def plot_regularization(results, tester, protocol, opt_index=5, yshift_b=0.,
-                        yshift_t=0.):
+def plot_regularization(results, tester, protocol, opt_index=5):
     sigma = tester.sigma
     vkwargs = dict(vmin=-3*sigma, vmax=3*sigma)
 
     res = [v['residual'] for k, v in results.items()]
+    vlim = {'vmin': np.min(res), 'vmax': np.max(res)}
     err = np.array([v['residual'].std() for k, v in results.items()])
     best = np.argmin(np.abs(err - sigma))
-
     lamb = np.array([p['sigma']/sigma for p in protocol])
-    fig, axe = plt.subplots(figsize=(6.4, 4.))
+
+    L = res[0].shape[0]
+    gs = gridspec.GridSpec(3, 4, height_ratios=[2, 1., 1.])
+
+    axe = plt.subplot(gs[0,:])
     axe.plot(lamb, err)
-    axe.axhline(sigma, c='k', label=r'True $\sigma$', lw=0.8)
+    axe.axhline(sigma, c='k', label=r'True $\sigma$', lw=0.8, dashes=[4,3])
     axe.set_ylabel(r'$\mathrm{std}(||Mg_\lambda - \phi||^2)$')
     axe.set_xlabel(r'Regularization strength $\lambda$')
     axe.legend(loc='lower right')
-   
-    axe.plot(lamb[0], err[0], '.', c='k')
-    im = OffsetImage(res[0], zoom=0.8, interpolation='nearest')
-    ab = AnnotationBbox(im, xy=[lamb[0], err[0]],
-                        xybox=(.31, .33-yshift_b), boxcoords='figure fraction',
-                        xycoords='data',
-                        pad=0.,
-                        arrowprops=dict(arrowstyle="->", lw=1.))
-    axe.add_artist(ab)
-    im = OffsetImage(absfft(res[0]), zoom=0.8, cmap='gray_r', interpolation='nearest')
-    ab = AnnotationBbox(im, xy=[lamb[0], err[0]],
-                        xybox=(0.35, 0.4-yshift_b), boxcoords='figure fraction',
-                        arrowprops=dict(alpha=0), pad=0)
-    axe.add_artist(ab)
 
-    axe.plot(lamb[best], err[best], '.', c='k')
-    im = OffsetImage(res[best], zoom=0.8, interpolation='nearest')
-    ab = AnnotationBbox(im, xy=[lamb[best], err[best]],
-                        xybox=(.535, .38-yshift_b), boxcoords='figure fraction',
-                        xycoords='data',
-                        pad=0.,
-                        arrowprops=dict(arrowstyle="->", lw=1.))
-    axe.add_artist(ab)
-    im = OffsetImage(absfft(res[best]), zoom=0.8, cmap='gray_r', interpolation='nearest')
-    ab = AnnotationBbox(im, xy=[lamb[0], err[0]],
-                        xybox=(0.575, 0.45-yshift_b), boxcoords='figure fraction',
-                        arrowprops=dict(alpha=0), pad=0)
-    axe.add_artist(ab)
+    axe.plot(lamb[0], err[0], marker='v', c='k', markersize=10.)
+    axe1 = plt.subplot(gs[1,0])
+    axe1.matshow(absfft(res[0]), cmap='gray_r')
+    axe1.plot(L/6, L/6, marker='v', c='k', markersize=10.)
+    axe1.axis('off')
+    axe1.text(-L/1.8, L/2, "Fourier\nresiduals",
+              fontdict={'rotation': 'vertical', 'verticalalignment': 'center'})
+    axe2 = plt.subplot(gs[2,0])
+    axe2.matshow(res[0], cmap='RdBu', **vlim)
+    axe2.plot(L/6, L/6, marker='v', c='k', markersize=10.)
+    axe2.text(-L/3.9, L/2, "Residuals",
+              fontdict={'rotation': 'vertical', 'verticalalignment': 'center'})
+    axe2.axis('off')
+    axe2.set_ylabel("Residuals")
 
-    axe.plot(lamb[-1], err[-1], '.', c='k')
-    im = OffsetImage(res[-1], zoom=0.8, interpolation='nearest')
-    ab = AnnotationBbox(im, xy=[lamb[-1], err[-1]],
-                        xybox=(.7675, .43-yshift_b), boxcoords='figure fraction',
-                        xycoords='data',
-                        pad=0.,
-                        arrowprops=dict(arrowstyle="->", lw=1.))
-    axe.add_artist(ab)
-    im = OffsetImage(absfft(res[-1]), zoom=0.8, cmap='gray_r', interpolation='nearest')
-    ab = AnnotationBbox(im, xy=[lamb[0], err[0]],
-                        xybox=(0.8075, 0.5-yshift_b), boxcoords='figure fraction',
-                        arrowprops=dict(alpha=0), pad=0)
-    axe.add_artist(ab)
+    axe.plot(lamb[opt_index], err[opt_index], marker='*', c='b', markersize=13.)
+    axe3 = plt.subplot(gs[1,1])
+    axe3.matshow(absfft(res[opt_index]), cmap='gray_r')
+    axe3.plot(L/6, L/6, marker='*', c='b', markersize=13.)
+    axe3.axis('off')
+    axe4 = plt.subplot(gs[2,1])
+    axe4.matshow(res[opt_index], cmap='RdBu', **vlim)
+    axe4.plot(L/6, L/6, marker='*', c='b', markersize=13.)
+    axe4.axis('off')
+    
+    axe.plot(lamb[best], err[best], marker='X', c='k', markersize=10.)
+    axe5 = plt.subplot(gs[1,2])
+    axe5.matshow(absfft(res[best]), cmap='gray_r')
+    axe5.plot(L/6, L/6, marker='X', c='k', markersize=10.)
+    axe5.axis('off')
+    axe6 = plt.subplot(gs[2,2])
+    axe6.matshow(res[best], cmap='RdBu', **vlim)
+    axe6.plot(L/6, L/6, marker='X', c='k', markersize=10.)
+    axe6.axis('off')
 
-    axe.plot(lamb[opt_index], err[opt_index], '.', c='k')
-    im = OffsetImage(res[opt_index], zoom=0.8, interpolation='nearest')
-    ab = AnnotationBbox(im, xy=[lamb[opt_index], err[opt_index]],
-                        xybox=(.245, .75-yshift_t), boxcoords='figure fraction',
-                        xycoords='data',
-                        pad=0.,
-                        arrowprops=dict(alpha=0))
-    axe.add_artist(ab)
-    im = OffsetImage(absfft(res[opt_index]), zoom=0.8, cmap='gray_r', interpolation='nearest')
-    ab = AnnotationBbox(im, xy=[lamb[opt_index], err[opt_index]],
-                        xybox=(0.295, 0.83-yshift_t), boxcoords='figure fraction',
-                        arrowprops=dict(arrowstyle='->', lw=1.), pad=0)
-    axe.add_artist(ab)
+    axe.plot(lamb[-1], err[-1], marker='H', c='k', markersize=10.)
+    axe7 = plt.subplot(gs[1,3])
+    axe7.matshow(absfft(res[-1]), cmap='gray_r')
+    axe7.plot(L/6, L/6, marker='H', c='k', markersize=10.)
+    axe7.axis('off')
+    axe8 = plt.subplot(gs[2,3])
+    axe8.matshow(res[-1], cmap='RdBu', **vlim)
+    axe8.plot(L/6, L/6, marker='H', c='k', markersize=10.)
+    axe8.axis('off')
 
-    axe.set_yticks([0.04, 0.045, 0.05, 0.055])
-    fig.subplots_adjust(bottom=0.15)
-    fig.subplots_adjust(left=0.15)
+    fig = plt.gcf()
+    fig.set_size_inches([5.3, 4.7])
 
-    return fig, axe 
+    p = axe.get_position()
+    axe.set_position([p.x0+0.075, p.y0+.1, p.width-.1, p.height-.03])
+    
+    return plt.gcf(), axe
 
 def compare_truth(uni_result, uni_tester, para_result, para_tester):
+    letters = string.ascii_lowercase
     fig = plt.figure(figsize=(10., 11.))
     L = len(uni_result)
     grid = ImageGrid(fig, 111, nrows_ncols=(4, 1 + L), axes_pad=0.1, 
@@ -133,7 +130,9 @@ def compare_truth(uni_result, uni_tester, para_result, para_tester):
     vlim = get_j_density_range(uni_tester.g, uni_result)
     uniform_truth = j_density(uni_tester.g)
     grid[0].matshow(uniform_truth, cmap='gray_r', **vlim)
-    grid[0].set_title("(a) True uniform $|\mathbf{j}|$", pad=0)
+    subfig = "({}) ".format(letters[0])
+    letters = letters[1:]
+    grid[0].set_title(subfig + "Ground truth\nuniform $|\mathbf{j}|$", pad=0)
 
     for i in range(len(grid)):
         if not i%(L+1) == L:
@@ -141,25 +140,31 @@ def compare_truth(uni_result, uni_tester, para_result, para_tester):
 
     for i, (label, res) in enumerate(uni_result.items()):
         im = grid[i+1].matshow(j_density(res['gsol']), cmap='gray_r', **vlim)
-        grid[i+1].set_title(label, pad=0)
+        subfig = "({}) ".format(letters[0])
+        letters = letters[1:]
+        grid[i+1].set_title(subfig + label, pad=0)
     grid.cbar_axes[L].colorbar(im)
 
-    vlim = get_j_density_range(para_tester.g, para_result, uniform_truth)
+    vlim = get_j_density_range(uni_tester.g, uni_result, uniform_truth)
     for i, (label, res) in enumerate(uni_result.items()):
         im = grid[L+i+2].matshow(j_density(res['gsol']) - uniform_truth, 
                                  cmap='RdBu', **vlim)
         if not i:
-            grid[L+i+2].set_title("Reconstruction \nerror", pad=0)
+            grid[L+i+2].set_title("Reconstruction error", pad=0)
     grid.cbar_axes[2 * L + 1].colorbar(im)
 
     vlim = get_j_density_range(para_tester.g, para_result)
     parabolic_truth = j_density(para_tester.g)
     grid[2*L+2].matshow(parabolic_truth, cmap='gray_r', **vlim)
-    grid[2*L+2].set_title(r"(e) True parabolic $|\mathbf{j}|$", pad=0)
+    subfig = "({}) ".format(letters[0])
+    letters = letters[1:]
+    grid[2*L+2].set_title(subfig + "Ground truth \nparabolic $|\mathbf{j}|$", pad=0)
 
     for i, (label, res) in enumerate(para_result.items()):
         im = grid[2*L+3+i].matshow(j_density(res['gsol']), cmap='gray_r', **vlim)
-        grid[2*L+3+i].set_title(label, pad=0)
+        subfig = "({}) ".format(letters[0])
+        letters = letters[1:]
+        grid[2*L+3+i].set_title(subfig + label, pad=0)
     grid.cbar_axes[3 * L + 2].colorbar(im)
 
     vlim = get_j_density_range(para_tester.g, para_result, parabolic_truth)
@@ -167,7 +172,7 @@ def compare_truth(uni_result, uni_tester, para_result, para_tester):
         im = grid[3*L+4+i].matshow(j_density(res['gsol'])-parabolic_truth, 
                                    cmap='RdBu', **vlim)
         if not i:
-            grid[3*L+4+i].set_title("Reconstruction \nerror", pad=0)
+            grid[3*L+4+i].set_title("Reconstruction error", pad=0)
     grid.cbar_axes[4 * L + 3].colorbar(im)
 
     for i in range(len(grid)):
@@ -248,11 +253,11 @@ protocols.append(
     dict(label="TV prior", decon="TVDeconvolver", sigma=TV_factor * sigma,
          deconv_kwargs=admm_kwargs)
 )
-#protocols.append(
-#    dict(label="TV and finite support prior",
-#         decon="TVDeconvolver", sigma=TV_factor * sigma, support_mask=mask,
-#         deconv_kwargs=admm_kwargs)
-#)
+protocols.append(
+    dict(label="TV and finite support prior",
+         decon="TVDeconvolver", sigma=TV_factor * sigma, support_mask=mask,
+         deconv_kwargs=admm_kwargs)
+)
 protocols.append(
     dict(label="gaussian prior", decon="LinearDeconvolver",
          sigma=L_factor * sigma)
@@ -262,59 +267,69 @@ protocols.append(
     dict(label="gaussian laplace prior", decon="LinearDeconvolver",
          sigma=L_factor * sigma, gamma=D2h + D2v)
 )
-#protocols.append(
-#    dict(label="gaussian and finite support prior",
-#         decon="LinearDeconvolver", 
-#         sigma=L_factor * sigma, support_mask=mask)
-#)
+protocols.append(
+    dict(label="gaussian and finite support prior",
+         decon="LinearDeconvolver", 
+         sigma=L_factor * sigma, support_mask=mask)
+)
 
 uniform_tester = Tester(g_uniform, kernel, sigma)
 parabolic_tester = Tester(g_parabolic, kernel, sigma)
 
 sigma = estimate_noise(uniform_tester.phi.reshape(kernel._shape))
 reg_protocol = []
-factors = np.linspace(0.0001, 10, 50)
+factors = np.linspace(0.0001, 10, 100)
 for fac in factors:
     reg_protocol.append(
         dict(label='factor = {}'.format(fac), decon='LinearDeconvolver',
-             sigma=fac * sigma, support_mask=mask, 
-             deconv_kwargs=dict(atol=1e-11, tol=1e-12))
+             sigma=fac * sigma, #support_mask=mask, 
+             deconv_kwargs=dict(solver='gcrotmk', atol=1e-10, tol=1e-10))
     )
 
-print("Performing uniform annulus tests")
-uniform_results = uniform_tester.test_protocols(protocols)
-
-print("Performing parabolic annulus tests")
-parabolic_results = parabolic_tester.test_protocols(protocols)
-
-fig, grid = compare_truth(
-    {
-        '(b) Gaussian \nLaplacian': uniform_results['gaussian laplace prior'],
-        '(c) Gaussian \nabsolute curvature': uniform_results['gaussian prior'],
-        '(d) TV \nabsolute curvature': uniform_results['TV prior'],
-        #'(d) Gaussian and FS prior': 
-        #  uniform_results['gaussian and finite support prior'],
-        #'(e) TV and FS prior': uniform_results['TV and finite support prior'],
-    },
-    uniform_tester, 
-    {
-        '(f) Gaussian \nLaplacian': parabolic_results['gaussian laplace prior'],
-        '(g) Gaussian \nabsolute curvature': parabolic_results['gaussian prior'],
-        '(h) TV \nabsolute curvature': parabolic_results['TV prior'],
-        #'(i) Gaussian and FS prior': 
-        #  parabolic_results['gaussian and finite support prior'],
-        #'(j) TV and FS prior': parabolic_results['TV and finite support prior'],
-    },
-    parabolic_tester
-)
+if False:
+    print("Performing uniform annulus tests")
+    uniform_results = uniform_tester.test_protocols(protocols)
+    
+    print("Performing parabolic annulus tests")
+    parabolic_results = parabolic_tester.test_protocols(protocols)
+    
+    fig, grid = compare_truth(
+        {
+            'Gaussian \nLaplacian': uniform_results['gaussian laplace prior'],
+            'Gaussian \nFrobenius': uniform_results['gaussian prior'],
+            'TV \nFrobenius': uniform_results['TV prior'],
+        },
+        uniform_tester, 
+        {
+            'Gaussian \nLaplacian': parabolic_results['gaussian laplace prior'],
+            'Gaussian \nFrobenius': parabolic_results['gaussian prior'],
+            'TV \nFrobenius': parabolic_results['TV prior'],
+        },
+        parabolic_tester
+    )
+    
+    fig, grid = compare_truth(
+        {
+            'Gaussian finite\nsupport (FS)': 
+              uniform_results['gaussian and finite support prior'],
+            'TV and FS': uniform_results['TV and finite support prior'],
+        },
+        uniform_tester, 
+        {
+            'Gaussian and FS': 
+              parabolic_results['gaussian and finite support prior'],
+            'TV and FS': parabolic_results['TV and finite support prior'],
+        },
+        parabolic_tester
+    )
 
 #print("Performing parabolic regularization test")
 #parabolic_reg_results = parabolic_tester.test_protocols(reg_protocol)
 #fig, axe = plot_regularization(parabolic_reg_results, parabolic_tester,
 #                               reg_protocol, 9, yshift_b=.02)
 
-#print("Performing uniform regularization test")
-#uniform_reg_results = uniform_tester.test_protocols(reg_protocol)
-#fig, axe = plot_regularization(uniform_reg_results, uniform_tester,
-#                               reg_protocol, 10, yshift_b=.03)
+print("Performing uniform regularization test")
+uniform_reg_results = uniform_tester.test_protocols(reg_protocol)
+fig, axe = plot_regularization(uniform_reg_results, uniform_tester,
+                               reg_protocol, 14)
 plt.show()
